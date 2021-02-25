330566c88db6d72643c0f8227ba4bcda54fa31af
Cool Guy Ruhaim <sulaimanbot370@gmail.com>
2021-02-25 22:23:20 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,17 +29,17 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made from the source code of [⌚️09:59:47 🔥@DontBelieveAnybody🔥 📅16.02.2021](t.me/DontBelieveAnybody)
 
-Special thanks to [this person](t.me/denver02) for helping me.
+Special thanks to [this person](t.me/Yash_MK) for helping me.
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
+Deploy a bot like me [now](www.github.com/xditya/GroupManager1)
 
 Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/CoolGuyRuhaimBot?startgroup=true).
 """
 
 
